e27dd28df6fb0330e9ce396b16c9dc5f8969cebf
ramirezr69 <patrickpogi5087@gmail.com>
2014-03-07 13:16:24 -0600

    Add Roll-a-ball tutorial

---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public GUIText countText;
+    public GUIText winText;
+    private int count;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
+        rigidbody.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp") //If player collides with pick up
+        {
+            other.gameObject.SetActive(false); //Destroy game object or deactivate it
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        {
+            if(count >= 12)
+                winText.text = "You Win!!";
+        }
+    }
+}
